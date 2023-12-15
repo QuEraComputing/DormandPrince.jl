ae16914e05f3079d30950716141037f0a3f7e6f4
John Long <johnzl@protonmail.ch>
2023-12-15 11:21:21 -0500

    Fix names and license

--- docs/make.jl
+++ docs/make.jl
@@ -8,14 +8,14 @@ makedocs(;
     modules = [DormandPrince],
     format = Documenter.HTML(
         prettyurls = !("local" in ARGS),
-        canonical="https://John Long.github.io/DormandPrince.jl",
+        canonical="https://queracomputing.github.io/DormandPrince.jl",
         assets=String[indigo],
     ),
     pages = [
         "Home" => "index.md",
     ],
-    repo = "https://github.com/John Long/DormandPrince.jl",
+    repo = "https://github.com/QuEraComputing/DormandPrince.jl",
     sitename = "DormandPrince.jl",
 )
 
-deploydocs(; repo = "https://github.com/John Long/DormandPrince.jl")
+deploydocs(; repo = "https://github.com/QuEraComputing/DormandPrince.jl")
