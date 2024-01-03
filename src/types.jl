412732d33137e36ca5f277159fe99aa30459c3b7
Phillip Weinberg <weinbe58@gmail.com>
2024-01-03 13:54:04 -0500

    adding interface for getting current state of integrator.

--- src/types.jl
+++ src/types.jl
@@ -213,3 +213,5 @@ function DP8Solver(
     DP8Solver(f, x, y, k1, k2, k3, k4, k5, k6, k7, k8, k9, k10, y1;kw...)
 end
 
+get_current_state(solver::DP5Solver) = solver.y
+get_current_state(solver::DP8Solver) = solver.y
