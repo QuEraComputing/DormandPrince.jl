56cd277005a77b31854e907ff2ff052538d72421
John Long <johnzl@protonmail.ch>
2024-01-11 12:56:59 -0500

    revised unit tests for pretty printing, properly trigger show

--- test/show.jl
+++ test/show.jl
@@ -1,21 +1,50 @@
 # show solver and report
 
 using DormandPrince
+using Test
 
 function stiff_fcn(x, y, f) 
     f[1] = y[1]^2 - y[1]^3
 end
 
-solver = DP5Solver(
-    stiff_fcn,
-    0.0,
-    [0.0001]
-    ;
-    maximum_allowed_steps=100
-)
+@testset "Successful Options Check with Successful Integration" begin
+    solver = DP5Solver(
+        stiff_fcn,
+        0.0,
+        [0.0001]
+    )
 
-show(solver)
+    show(stdout, MIME"text/plain"(), solver)
 
-report = DormandPrince.integrate_core!(solver, 2/0.0001)
+    report = DormandPrince.integrate_core!(solver, 2/0.0001)
 
-show(report)
+    show(stdout, MIME"text/plain"(), report)
+    
+end
+
+@testset "Successful Options Check with Failed Integration" begin
+    solver = DP5Solver(
+        stiff_fcn,
+        0.0,
+        [0.0001];
+        maximum_allowed_steps=10 # 
+    )
+
+    report = DormandPrince.integrate_core!(solver, 2/0.0001)
+
+    show(stdout, MIME"text/plain"(), report)
+end
+
+@testset "Failed Options Check" begin
+    solver = DP5Solver(
+        stiff_fcn,
+        0.0,
+        [0.0001];
+        uround = 100
+    )
+
+    report = DormandPrince.integrate_core!(solver, 2/0.0001)
+
+    show(stdout, MIME"text/plain"(), report)
+
+end
